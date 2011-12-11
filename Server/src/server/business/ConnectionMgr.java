c4d8b5f0e3f549d5346d685f1d39f4f8fdbfdf8f
Jason Mansfield <trentonknight@gmail.com>
2011-12-10 23:18:08 -0500

    server might be done?

--- Server/src/server/business/ConnectionMgr.java
+++ Server/src/server/business/ConnectionMgr.java
@@ -1,25 +1,49 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package server.business;
-
-import java.io.*;
 import java.net.*;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-/**
- *
- * @author trentonknight
- */
+ 
 public class ConnectionMgr {
-   
-        Socket socket = null;
+    public static void main(String[] args) throws IOException {
+ 
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
         ObjectInputStream in = null;
         ObjectOutputStream out = null;
         boolean exit = false;
-      
-               
-       
+        while(!exit){
+            clientSocket = serverSocket.accept();
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            in = new ObjectInputStream(clientSocket.getInputStream());
+            try {
+                String inputStr = (String)in.readObject();
+                if(!inputStr.equals("exit")){
+                out.writeObject("Hello" + inputStr);
+            }
+                else{
+                    exit = true;
+                    out.writeObject("Exiting");
+                }
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(ConnectionMgr.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           in.close();
+           out.close();
+           clientSocket.close();
+        }
+        serverSocket.close();
+    }
 }
